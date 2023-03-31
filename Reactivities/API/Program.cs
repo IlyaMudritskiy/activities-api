093e6270a42ff2636845cbe8bed86e2cd150bb28
Ilya Mudritskiy <ilya.mudritskiy@gmail.com>
2023-03-31 17:01:57 +0100

    Updated comments

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -4,19 +4,29 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
+//==============================================================================
+//=                                  Builder                                   =
+//==============================================================================
 var builder = WebApplication.CreateBuilder(args);
 
+//==============================================================================
+//=                              Default Services                              =
+//==============================================================================
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-/* DB Context with SQLite */
+//==============================================================================
+//=                           DB Context for SQLite                            =
+//==============================================================================
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-/* Cors policy */
+//==============================================================================
+//=                                Cors policy                                 =
+//==============================================================================
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policy =>
@@ -25,31 +35,43 @@ builder.Services.AddCors(options =>
     });
 });
 
+//==============================================================================
+//=                              Custom Services                               =
+//==============================================================================
 builder.Services.AddMediatR(typeof(List.Handler));
-
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
+//==============================================================================
+//=                                    App                                     =
+//==============================================================================
 var app = builder.Build();
 
-/* HTTP request pipeline */
+//==============================================================================
+//=                      Configure HTTP request pipeline                       =
+//==============================================================================
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//==============================================================================
+//=                                  App Use                                   =
+//==============================================================================
 app.UseCors("CorsPolicy");
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-/* Scope */
+//==============================================================================
+//=                                   Scope                                    =
+//==============================================================================
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
+//==============================================================================
+//=                        Migrations and seedind data                         =
+//==============================================================================
 try
 {
     var context = services.GetRequiredService<DataContext>();
@@ -62,4 +84,7 @@ catch (Exception exception)
     logger.LogError(exception, "An error occurred during migration");
 }
 
+//==============================================================================
+//=                                    Run                                     =
+//==============================================================================
 app.Run();
