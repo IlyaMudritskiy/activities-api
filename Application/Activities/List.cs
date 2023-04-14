057dc778c82c4024ce19fb31b91e264d81a9713f
Ilya Mudritskiy <ilya.mudritskiy@gmail.com>
2023-04-14 22:43:04 +0100

    Added AutoMapper profiles for List and Details

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,6 @@
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -25,13 +26,10 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDTO>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .Include(a => a.Attendees)
-                    .ThenInclude(u => u.AppUser)
+                    .ProjectTo<ActivityDTO>(_mapper.ConfigurationProvider)
                     .ToListAsync(cancellationToken);
-
-                var activitiesToReturn = _mapper.Map<List<ActivityDTO>>(activities);
-
-                return Result<List<ActivityDTO>>.Success(activitiesToReturn);
+                    
+                return Result<List<ActivityDTO>>.Success(activities);
             }
         }
     }
